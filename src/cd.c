ac62fd0b85d527f126509fdb6df5976c2cf7b16d
Kaan Karakus <kaan.karakus@outlook.com.tr>
2024-07-21 16:39:55 +0300

    Parser & lexer

---
+++ src/cd.c
@@ -0,0 +1,30 @@
+#include "minishell.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int	exec_cd(t_state *state, t_token *token)
+{
+	const char	*home;
+
+	if (!token)
+		return (FAILURE);
+	if (!token->next)
+	{
+		home = env_get_value(state, "HOME");
+		if (!home)
+			return (print_exec_err(state, token, 1, ERR_HOME_NOT_SET));
+		if (chdir(home) == -1)
+			return (print_exec_err(state, token, 114, ERR_CANT_CHANGE_DIR));
+		if (env_set_pwd(state) != SUCCESS)
+			return (FAILURE);
+		state->status = 0;
+		return (SUCCESS);
+	}
+	if (chdir(token->next->data) == -1)
+		return (print_exec_err(state, token, 1, ERR_NO_SUCH_FILE_OR_DIR));
+	if (env_set_pwd(state) != SUCCESS)
+		return (FAILURE);
+	state->status = 0;
+	return (SUCCESS);
+}
---
+++ src/exec_single.c
@@ -0,0 +1,70 @@
+#include "minishell.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int	exec_single_cmd_with_fork(t_cmd *cmd, t_state *state)
+{
+	pid_t	pid;
+
+	g_sig = IN_CMD;
+	pid = fork();
+	if (pid == -1)
+		return (FAILURE);
+	else if (pid == 0)
+	{
+		if (cmd->in != NAFD)
+			dup2(cmd->in, STDIN_FILENO);
+		if (cmd->out != NAFD)
+			dup2(cmd->out, STDOUT_FILENO);
+		if (execve(cmd->cmd, cmd->argv, state->env) == -1)
+			exit(state->status);
+	}
+	if (pid != 0)
+	{
+		waitpid(pid, &state->status, 0);
+		state->status = w_exit_status(state->status);
+	}
+	g_sig = AFTER_CMD;
+	return (SUCCESS);
+}
+
+int	exec_single_cmd_prepare_fds(t_token *token, t_state *state, t_cmd *cmd)
+{
+	if (set_heredoc_fds(token, cmd, 0) != SUCCESS)
+		return (FAILURE);
+	if (set_red_file_fds(token, cmd, state) != SUCCESS)
+		return (FAILURE);
+	if (token_has_cmd(token) == false)
+		return (SUCCESS);
+	if (set_cmd_arg_and_path(token, state, cmd, NULL) != SUCCESS)
+		return (FAILURE);
+	if (!cmd->cmd)
+		return (FAILURE);
+	if (cmd->in == NAFD)
+		cmd->in = cmd->heredoc[0];
+	return (SUCCESS);
+}
+
+void	cmd_dispose(t_cmd *cmd)
+{
+	if (!cmd)
+		return ;
+	free(cmd->cmd);
+	cmd->cmd = NULL;
+	free(cmd->argv);
+	cmd->argv = NULL;
+	free(cmd->heredoc);
+	cmd->heredoc = NULL;
+}
+
+int	exec_single_cmd(t_token *token, t_state *state, t_cmd *cmd)
+{
+	if (!token || !state || !cmd)
+		return (FAILURE);
+	if (exec_single_cmd_prepare_fds(token, state, cmd) != SUCCESS)
+		return (cmd_dispose(cmd), FAILURE);
+	if (exec_single_cmd_with_fork(cmd, state) != SUCCESS)
+		return (cmd_dispose(cmd), FAILURE);
+	return (cmd_dispose(cmd), SUCCESS);
+}

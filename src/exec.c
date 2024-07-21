23d268010abc2979604c8b0eb1bbdafa63df2730
Ahmethsnl <asanli018@gmail.com>
2024-07-21 21:26:55 +0300

    organizin exec.c

--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: ahmsanli <ahmsanli@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 21:09:52 by ahmsanli          #+#    #+#             */
-/*   Updated: 2024/07/21 21:23:59 by ahmsanli         ###   ########.fr       */
+/*   Updated: 2024/07/21 21:26:46 by ahmsanli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,23 @@ void which_command_built_in(t_state *state)
 	int i;
 
 	i = 0;
-		while (state->token_arr[i])
-		{
-			if (ft_strcmp(state->token_arr[i], "echo") == 0)
-				run_echo(state);
-			else if (ft_strcmp(state->token_arr[i], "cd") == 0)
-				run_cd(state);
-			else if (ft_strcmp(state->token_arr[i], "pwd") == 0)
-				run_pwd(state);
-			else if (ft_strcmp(state->token_arr[i], "export") == 0)
-				run_export(state);
-			else if (ft_strcmp(state->token_arr[i], "unset") == 0)
-				run_unset(state);
-			else if (ft_strcmp(state->token_arr[i], "env") == 0)
-				run_env(state);
-			else if (ft_strcmp(state->token_arr[i], "exit") == 0)
-				run_exit(state);
-			i++;
-		}
+	while (state->token_arr[i])
+	{
+		if (ft_strcmp(state->token_arr[i], "echo") == 0)
+			run_echo(state);
+		else if (ft_strcmp(state->token_arr[i], "cd") == 0)
+			run_cd(state);
+		else if (ft_strcmp(state->token_arr[i], "pwd") == 0)
+			run_pwd(state);
+		else if (ft_strcmp(state->token_arr[i], "export") == 0)
+			run_export(state);
+		else if (ft_strcmp(state->token_arr[i], "unset") == 0)
+			run_unset(state);
+		else if (ft_strcmp(state->token_arr[i], "env") == 0)
+			run_env(state);
+		else if (ft_strcmp(state->token_arr[i], "exit") == 0)
+			run_exit(state);
+		i++;
+	}
 }
 */

8d3accd718eaf0386c8c1781d95d5921ec59a332
Ahmethsnl <asanli018@gmail.com>
2024-08-16 20:45:19 +0300

    oldpwd not set problemi çözülmek üzere

--- src/cd.c
+++ src/cd.c
@@ -6,7 +6,7 @@
 /*   By: ahmsanli <ahmsanli@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 18:11:08 by ahmsanli          #+#    #+#             */
-/*   Updated: 2024/08/16 18:18:38 by ahmsanli         ###   ########.fr       */
+/*   Updated: 2024/08/16 20:44:44 by ahmsanli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,32 @@ static int	old_path_util(t_state *state, char *old_path)
 	return (SUCCESS);
 }
 
-static int	home_set(t_state *state, char *home, t_token *token)
+static int	cd_with_flag(t_state *state, t_token *token, bool is_home_set)
 {
+	char *path;
+
+	if (ft_strncmp(token->next->data, "-", 2) == 0)
+	{
+		if (is_home_set == false)
+		{
+			path = ft_strdup("OLDPWD=");
+			print_execute_err(state, token, 1, ERR_OLDPWD_NOT_SET);
+			env_set_value(state, path);
+			return (SUCCESS);
+		}
+		is_home_set = true;
+	}
+	return (SUCCESS);
+}
+
+static int	home_set(t_state *state, char *home, t_token *token, bool is_home_set)
+{
+	if (cd_with_flag(state, token, is_home_set))
+		return (SUCCESS);
 	if (!token->next)
 	{
-		if (!home)
-			return (print_execute_err(state, token, 1, ERR_HOME_NOT_SET));
 		if (chdir(home) == -1)
-			return (print_execute_err(state, token, 1, ERR_CANT_CHANGE_DIR));
+			return (print_execute_err(state, token, 0, ERR_HOME_NOT_SET), SUCCESS);
 		if (env_set_pwd(state) != SUCCESS)
 			return (FAILURE);
 		state->status = 0;
@@ -51,12 +69,14 @@ int	run_cd(t_state *state, t_token *token)
 {
 	char	*home;
 	char	*old_path;
+	bool	is_home_set;
 
 	old_path = getcwd(NULL, 0);
+	is_home_set = false;
 	if (!token)
 		return (FAILURE);
 	home = get_env_value(state, "HOME");
-	if (home_set(state, home, token) != SUCCESS)
+	if (home_set(state, home, token, is_home_set) != SUCCESS)
 		return (FAILURE);
 	if (chdir(token->next->data) == -1)
 		return (print_execute_err(state, token, 1, ERR_NO_SUCH_FILE_OR_DIR));
